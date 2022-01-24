2468f7949f07508a444e9c7c9b9d8c0e89cfd5e2
Aidan Pierce <apierce@thecargoagency.com>
2022-01-24 14:51:23 -0500

    used webdriver wait instead of sleep to wait for content to load before executing instead of by hardcoded time. This makes it much faster and eliminates possibility of a failed script if connection is too slow to load page in time

--- working/awareness-160x600/main.py
+++ working/awareness-160x600/main.py
@@ -6,38 +6,37 @@ from selenium.webdriver.common.by import By
 import time
 from PIL import Image
 import pathlib
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 # FOR LOOP TO ADD A main.py TO EACH FOLDER
 # for d in */ ; do cp main.py $d/ done
 
-# FOR LOOP TO RUN EACH main.py FILE one directory deep
-# for d in */ ; do cd $d/ python3 main.py cd ../ done
-
-# FOR LOOP TO RUN EACH main.py FILE two directories deep
-# for d in */ ; do cd $d/ for i in */ ; sdo cd $i/ python3 main.py cd ../ done cd ../ done
-
-# browser says were using chrome for webdriver and links to path I have chromedriver file
+# browser says were using chrome for webdriver headlessly (you dont see the actions happen. This is faster and you can use computer while tasks happen in bg)
 options = Options()
 options.headless = True
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+# current path is the file path of where this py file is located
 current_path = pathlib.Path(__file__).parent.absolute()
 
+# html path is the path to the html file in the same folder as your py file
 html_path = str(current_path).replace("main.py", "index.html", 1)
 
+# opens html banner file
 browser.get('file://' + html_path + '/index.html')
 
-time.sleep(3)
+# waits to execute next line until the opened page has the element gwd-page loaded (if you dont wait it will run immmediatley and not be able to find the element and not work. If you manually sleep it will add extra time)
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.TAG_NAME, "gwd-page")))
 
-search_input = browser.find_element(By.TAG_NAME, "gwd-page")
+# sets banner_area to the banner by finding the tag gwd-page in the html
+banner_area = browser.find_element(By.TAG_NAME, "gwd-page")
 
-time.sleep(3)
+# wait for text to load
+time.sleep(.5)
 
-# types the parameter string in the google search input
-search_input.screenshot(html_path + '/backup.jpg')
-
-time.sleep(2)
+# screenshots just the banner_area, and saves it as backup.jpg
+banner_area.screenshot(html_path + '/backup.jpg')
 
 browser.quit()
-
