2680e7d7bfd39353d6e805f46873a56616e115ba
Aidan Pierce <apierce@thecargoagency.com>
2022-01-25 15:11:47 -0500

    removed hardcoded paths, replaced python path commands with var to make code cleaner

--- run_all_pys.py
+++ run_all_pys.py
@@ -9,21 +9,21 @@ import pathlib
 
 # this script places the py file to create backup images in every banner folder, then it runs every py file to create the backup img
 
-# root_dir needs a trailing slash (i.e. /root/dir/)
-root_dir = str(pathlib.Path().resolve()) + '/' + next(os.walk('.'))[1][0] + '/'
+# banner_dir needs a trailing slash path of where your banners are. Needs to be a directory and the only directory in the same level as your run_all_pys file. The pathlib path resolve finds the path to where you are running this py file. The os walk finds the directory in this path, and the slashes are to make sure its written as a trailing slash so it can execute.
+root_dir = str(pathlib.Path().resolve())
+
+banner_dir = root_dir + '/' + next(os.walk('.'))[1][0] + '/'
 
 # filenamehtml for loop places the main.py file in every folder there is an html (so every banner directory will now have the main.py file in it, the main.py file deletes itself after the script runs so you should never see it but if the backup img appears it works :) )
-for filenamehtml in glob.iglob(root_dir + '**/**.html', recursive=True):
-    source = "/Users/apierce/Code/python-google-automation/src/backup_img/backup-img-banner/main.py"
+for filenamehtml in glob.iglob(banner_dir + '**/**.html', recursive=True):
+    source = root_dir + "/main.py"
     destination = filenamehtml.replace("index.html", "main.py")
     shutil.copy( source , destination )
 
-time.sleep(3)
-
 # filenamepy runs every py file in current directory. these py files are the scripts that create the backup img.
-for filenamepy in glob.iglob(root_dir + '**/**.py', recursive=True):
-    isCopyMain = filenamepy == "/Users/apierce/Code/python-google-automation/src/backup_img/backup-img-banner/main.py"
-    isRunPy = filenamepy == "/Users/apierce/Code/python-google-automation/src/backup_img/backup-img-banner/run_all_pys.py"
+for filenamepy in glob.iglob(banner_dir + '**/**.py', recursive=True):
+    isCopyMain = filenamepy == root_dir + "/main.py"
+    isRunPy = filenamepy == root_dir + "/run_all_pys.py"
     if not isCopyMain or not isRunPy:
         def execfile(filepath, globals=None, locals=None):
             if globals is None:
